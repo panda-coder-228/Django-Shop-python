eb069cfcdc35bf94fd9bf9ebbd6d0fdb769c4b17
DIMA GAVRIK <dimagavrik158@gmail.com>
2026-07-07 17:12:54 +0300

    Add admin_main. Registration in the admin (model:Category, Product, ProductImageInline)

--- apps/main/admin.py
+++ apps/main/admin.py
@@ -1,3 +1,147 @@
 from django.contrib import admin
+from .models import Category, Product, ProductImage
+from django.utils.html import format_html
+from django.urls import reverse
 
-# Register your models here.
+
+admin.site.site_header = "Glyse django shop"
+admin.site.site_title = "My shop"
+admin.site.index_title = "Welcome to the administration"
+
+@admin.action(description="Зробити товар доступним")
+def make_available(modeladmin, request, queryset):
+    queryset.update(available=True)
+
+@admin.action(description="Зробити товар не доступним")
+def make_anavailbale(modeladmin, request, queryset):
+    queryset.update(availble=False)
+
+@admin.action(description="Убрати скидку")
+def reset_discount(modeladmin, request, queryset):
+    queryset.update(discount=0)
+
+
+class ProductImageInline(admin.TabularInline):
+        model = ProductImage
+        extra = 2 
+        ordering = ('number_position',)
+        fields = [
+                'preview',
+                'image',
+                'number_position'
+        ]
+        readonly_fields = ['preview']
+
+        def preview(self, obj):
+            if obj and obj.image:
+                return format_html(
+                     '<img class="inline-preview-img" src={} width="70" style="border-radious:6px; object-fit:cover;"',
+                     obj.image.url
+                )
+            return format_html(
+                 '<img class="inline-preview-img" src={} width="80" style="display:none, border-radius:6px"><br>'
+                 '<span class="no-photo-text" style="color:#888; font-style:italic;"> нету фото </span> '
+            )
+        
+        preview.short_description = "Фото товара"
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'image_category', 'slug',)
+    search_fields = ('title',)
+    prepopulated_fields = {'slug': ('title','slug',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    class Media:
+        js = ("admin/js/image_preview.js/",)
+
+    list_display = ('slug', 'title', 'description', 'available', 'price', 'discount', 'created_at', 'updated_at',)
+    list_editable = ('price', 'available', 'description',)
+    search_fields = ('title__category', 'title', 'slug', 'created_at',)
+    list_filter = ( 'created_at', 'updated_at',)
+    list_per_page = 10
+    inlines = [ProductImageInline]
+    actions = [make_anavailbale, make_available, reset_discount]
+    fieldsets = [
+         ("Основна",{'fields': ['image_tag','title', 'slug', 'category', 'final_price',  'delete_link']}),
+         ("Зображення", {'fields': ['image', "main_image_preview"]}),
+         ("Ціна", {'fields': ['price', 'discount']}),
+         ("Статус",{'fields': ['available']}),
+         ("Параметри системи", {'fields':['created_at', 'updated_at'], 'classes':('collapse', )})
+    ]
+    readonly_fields = [
+         'main_image_preview',
+         'created_at',
+         'updated_at',
+    ]
+
+
+    def main_image_preview(self, obj):
+        if obj and obj.image:
+            return format_html(
+            '<img id="main-image-preview" src="{}" '
+            'style="width:120px; height:150px; '
+            'object-fit:cover; '
+            'border-radius:8px; '
+            'box-shadow:0 4px 10px rgba(0,0,0,0.25); '
+            'transition: opacity 0.3s ease; '
+            'opacity:1;">',
+            obj.image.url,
+            )
+
+        return format_html(
+            '<div class="main-image-container">'
+            '<img id="main-image-preview" '
+            'style="display:none; width:120px; height:150px; '
+            'object-fit:cover; '
+            'border-radius:8px; '
+            'box-shadow:0 4px 10px rgba(0,0,0,0.25); '
+            'transition: opacity 0.3s ease; '
+            'opacity:0;">'
+            '<br><span class="no-photo-text" '
+            'style="color:#888; font-style:italic;">Нет фото</span>'
+            '</div>'
+            )
+
+    def image_tag(self, obj):
+        if obj.image:
+            url = reverse("admin:main_product_change", args=[obj.pk])
+            return format_html(
+                '<a href="{}" >'
+                '<img src="{}" width="50" height="60" style="border-radius:6px;"/>'
+                '</a>',
+                url,
+                obj.image.url
+            )
+        return "-"
+    
+    image_tag.short_description = "Image"
+    image_tag.admin_order_field = "Image"
+
+    def delete_link(self, obj):
+        if obj.pk:
+            url = reverse("admin:main_product_delete", args=[obj.pk])
+            return format_html(
+                '<a href="{}" >Удалить</a>', url
+            )
+        return "-"
+    
+    delete_link.short_description = "Удалить"
+
+    def final_price(self, obj):
+        final_price = obj.sell_price
+        if obj.discount > 0:
+            return format_html (
+                '<span style="color:#d9534f; font-weight:600;"> {} грн</span>', final_price
+            )
+        else:
+            return format_html (
+                '<span style="color:#5cb85c; font-weight:600"> {} грн</span>', obj.price
+            )
+    
+    final_price.short_description = "Final_price"
+    final_price.admin_order_field = "price"
--- apps/main/models.py
+++ apps/main/models.py
@@ -2,23 +2,23 @@ from django.db import models
 from decimal import Decimal
 from django.utils.text import slugify
 from django.urls import reverse
-from main.utils import category_image_path
+from apps.main.utils import category_image_path
 
 
 
 class Category(models.Model):
-    title = models.CharField("Категория", max_length=50, unique=True)
-    image_category = models.ImageField("Фото для категории", upload_to=category_image_path(), blank=True, null=True)
+    title = models.CharField("Каталог", max_length=50, unique=True)
+    image_category = models.ImageField("Фото для категории", upload_to=category_image_path, blank=True, null=True)
     slug = models.SlugField(max_length=50, unique=True)
 
 
     class Meta:
         ordering = ['title']
-        verbose_name = "category"
-        verbose_name_plural = "categories"
+        verbose_name = "Категорія"
+        verbose_name_plural = "Категорії"
 
     def __str__(self):
-        return self.title
+        return f"Назва категорії: {self.title}"
     
     def get_absolute_url(self):
         return reverse("main:product_list_by_categories", args=(self.slug,))
@@ -34,5 +34,57 @@ class Category(models.Model):
                 num += 1
             self.slug = slug
         super().save(*args, **kwargs)
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name="products", blank=True, null=True, verbose_name="Категорія", on_delete=models.CASCADE)
+    title = models.CharField("Назва продукту", max_length=50, blank=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    image = models.ImageField("Фото продукту", upload_to="main_image_product/product/%Y/%m/", blank=True, null=True)
+    description = models.TextField("Опис продукту", blank=True)
+    available = models.BooleanField("Наличия товара",default=True)
+    price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
+    discount = models.DecimalField("Скидка%", max_digits=4, decimal_places=2, default=0.0)
+    created_at = models.DateTimeField("Дата створення", auto_now_add=True)
+    updated_at = models.DateTimeField("Оновленно", auto_now=True)
+
+
+    class Meta:
+        ordering = ['-created_at']
+        indexes = [
+            models.Index(fields=['slug', 'title']),
+            models.Index(fields=['title']),
+            models.Index(fields=['-created_at']),
+        ]
+        verbose_name = "Товар"
+        verbose_name_plural = "Товари"
+
+    def __str__(self):
+        return f"Назва товара: {self.title}"
     
+    def get_absolute_url(self):
+        return reverse("main:product_detail", args=[self.slug])
     
+    @property
+    def sell_price(self):
+        if self.discount:
+            return self.price - (self.price * self.discount) / Decimal("100")
+        return self.price
+    
+    @property
+    def get_discount_percent(self):
+        return round(self.discount, 0)
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_images")
+    images = models.ImageField("Фото товарів", upload_to="product/images/", blank=True, null=True)
+    number_position = models.PositiveIntegerField(default=True)
+
+    class Meta:
+        ordering = ('number_position',)
+        verbose_name = "Фотографія товара"
+        verbose_name_plural = "Фотографії товарів"
+
+    def __str__(self):
+        return f"Фотография товара: {self.product.title}"
