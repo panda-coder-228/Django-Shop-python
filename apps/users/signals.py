58831d27e4d13ed2f76193d09818698d8f6c2004
DIMA GAVRIK <dimagavrik158@gmail.com>
2026-07-13 01:26:10 +0300

    update file signals.py/ auto add customer

--- apps/users/signals.py
+++ apps/users/signals.py
@@ -3,18 +3,32 @@ from django.dispatch import receiver
 from django.contrib.auth.models import Group
 from django.conf import settings
 
+
+User = settings.AUTH_USER_MODEL
+
+
 @receiver(post_migrate)
 def create_user_group(sender, **kwargs):
     """Перевірка  для app users/ щоб не повторявалось , при migrations"""
     if sender.name != "apps.users":
         return
     
-    gropups = [
+    groups = [
         "Customer",
         "Manager",
         "Admin",
     ]
 
-    for group_name in gropups:
+    for group_name in groups:
         Group.objects.get_or_create(name=group_name)
     
+
+@receiver(post_save, sender=User)
+def add_customer_group(sender, instance, created, **kwargs):
+    """"Авт добовляет пользователя в Customer"""
+    if created:
+        customer_group = Group.objects.get(
+            name = "Customer"
+        )
+
+        instance.groups.add(customer_group)
