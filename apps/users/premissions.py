bf482e756bfa8cac2e1cf0eef317607cac44eefd
DIMA GAVRIK <dimagavrik158@gmail.com>
2026-07-13 12:04:00 +0300

    add premission for customer, manager

---
+++ apps/users/premissions.py
@@ -0,0 +1,46 @@
+CUSTOMER_PREMISSIONS = {
+    "main": [
+        "view_product"
+    ],
+    # "orders":[
+    #     "add_order",
+    #     "view_order",
+    # ],
+    "users":[
+        "view_customeruser",
+        "change_customeruser",
+        ""
+    ]
+}
+
+MANAGER_PREMISSINS = {
+    "main":[
+        "add_product",
+        "change_product",
+        "delete_product",
+        "view_product",
+
+        "add_category",
+        "change_category",
+        "delete_category",
+        "view_category"
+
+        "add_productimage",
+        "change_productimage",
+        "delete_productimage",
+        "view_productimage",
+    ],
+    # "orders":[
+    #     "add_order",
+    #     "change_order",
+    #     "delete_order",
+    #     "view_order",
+    #     "view_orderitem"
+    # ],
+    # "werahouse":[
+    #     "add_warehouse"
+    #     "delete_warehouse"
+    #     "view_warehouse"
+    #     "change_warehouse"
+    # ]
+}
